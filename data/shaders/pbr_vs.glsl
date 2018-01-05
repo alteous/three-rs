three-rs/three

Use samplerBuffer for joint matrices

data/shaders/pbr_vs.glsl
@@ -35,25 +35,40 @@ out vec2 v_TexCoord;
 out mat3 v_Tbn;
 out vec3 v_Normal;
 
+uniform samplerBuffer b_JointTransforms;
+
+mat4 fetch_joint_transform(int i)
+{
+    vec4 col0 = texelFetch(b_JointTransforms, 4 * i);
+    vec4 col1 = texelFetch(b_JointTransforms, 4 * i + 1);
+    vec4 col2 = texelFetch(b_JointTransforms, 4 * i + 2);
+    vec4 col3 = texelFetch(b_JointTransforms, 4 * i + 3);
+
+    return mat4(col0, col1, col2, col3);
+}
+
+mat4 compute_skin_transform()
+{
+    return
+	a_Weight.x * fetch_joint_transform(int(a_Joint.x)) +
+	a_Weight.y * fetch_joint_transform(int(a_Joint.y)) +
+	a_Weight.z * fetch_joint_transform(int(a_Joint.z)) +
+	a_Weight.w * fetch_joint_transform(int(a_Joint.w));
+}
+
 void main()
 {
-    mat4 u_Model = u_World;
-    mat4 u_Mvp = u_ViewProj * u_World;
+    mat4 mx_mvp = u_ViewProj * u_World;
+    mat4 mx_skin = compute_skin_transform();
 
-    vec4 position = u_Model * a_Position;
-    vec3 normal = normalize(vec3(u_Model * vec4(a_Normal.xyz, 0.0)));
-    vec3 tangent = normalize(vec3(u_Model * vec4(a_Tangent.xyz, 0.0)));
+    vec4 position = u_World * a_Position;
+    vec3 normal = normalize(vec3(u_World * vec4(a_Normal.xyz, 0.0)));
+    vec3 tangent = normalize(vec3(u_World * vec4(a_Tangent.xyz, 0.0)));
     vec3 bitangent = cross(normal, tangent) * a_Tangent.w;
 
     v_Tbn = mat3(tangent, bitangent, normal);
     v_Position = vec3(position.xyz) / position.w;
     v_TexCoord = a_TexCoord;
 
-    mat4 skin_transform =
-	a_Weight.x * u_JointMatrix[int(a_Joint.x)] +
-	a_Weight.y * u_JointMatrix[int(a_Joint.y)] +
-	a_Weight.z * u_JointMatrix[int(a_Joint.z)] +
-	a_Weight.w * u_JointMatrix[int(a_Joint.w)];
-
-    gl_Position = u_Mvp * skin_transform * a_Position;
+    gl_Position = mx_mvp * mx_skin * a_Position;
 }

alteous/three-rs

data/shaders/pbr_vs.glsl
@@ -41,26 +41,41 @@ in vec4 i_MatParams;
 in vec4 i_Color;
 in vec4 i_UvRange;
 
+uniform samplerBuffer b_JointTransforms;
+
+mat4 fetch_joint_transform(int i)
+{
+    vec4 col0 = texelFetch(b_JointTransforms, 4 * i);
+    vec4 col1 = texelFetch(b_JointTransforms, 4 * i + 1);
+    vec4 col2 = texelFetch(b_JointTransforms, 4 * i + 2);
+    vec4 col3 = texelFetch(b_JointTransforms, 4 * i + 3);
+
+    return mat4(col0, col1, col2, col3);
+}
+
+mat4 compute_skin_transform()
+{
+    return
+	a_Weight.x * fetch_joint_transform(int(a_Joint.x)) +
+	a_Weight.y * fetch_joint_transform(int(a_Joint.y)) +
+	a_Weight.z * fetch_joint_transform(int(a_Joint.z)) +
+	a_Weight.w * fetch_joint_transform(int(a_Joint.w));
+}
+
 void main()
 {
-    mat4 m_World = transpose(mat4(i_World0, i_World1, i_World2, vec4(0.0, 0.0, 0.0, 1.0)));
-    mat4 u_Model = m_World;
-    mat4 u_Mvp = u_ViewProj * m_World;
+    mat4 mx_world = transpose(mat4(i_World0, i_World1, i_World2, vec4(0.0, 0.0, 0.0, 1.0)));
+    mat4 mx_mvp = u_ViewProj * mx_world;
+    mat4 mx_skin = compute_skin_transform();
 
-    vec4 position = u_Model * a_Position;
-    vec3 normal = normalize(vec3(u_Model * vec4(a_Normal.xyz, 0.0)));
-    vec3 tangent = normalize(vec3(u_Model * vec4(a_Tangent.xyz, 0.0)));
+    vec4 position = mx_world * a_Position;
+    vec3 normal = normalize(vec3(mx_world * vec4(a_Normal.xyz, 0.0)));
+    vec3 tangent = normalize(vec3(mx_world * vec4(a_Tangent.xyz, 0.0)));
     vec3 bitangent = cross(normal, tangent) * a_Tangent.w;
 
     v_Tbn = mat3(tangent, bitangent, normal);
     v_Position = vec3(position.xyz) / position.w;
     v_TexCoord = a_TexCoord;
 
-    mat4 skin_transform =
-	a_Weight.x * u_JointMatrix[int(a_Joint.x)] +
-	a_Weight.y * u_JointMatrix[int(a_Joint.y)] +
-	a_Weight.z * u_JointMatrix[int(a_Joint.z)] +
-	a_Weight.w * u_JointMatrix[int(a_Joint.w)];
-
-    gl_Position = u_Mvp * skin_transform * a_Position;
+    gl_Position = mx_mvp * mx_skin * a_Position;
 }